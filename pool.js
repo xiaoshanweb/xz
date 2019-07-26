f22af61f0ae798a9de8afc5305e6c4f2789d1b66
xs <2681484014@qq.com>
2019-07-26 08:45:37 +0800

    连接池

---
+++ pool.js
@@ -0,0 +1,12 @@
+//创建连接池对象
+const mysql=require('mysql');
+var pool=mysql.createPool({
+	host:'127.0.0.1',
+	port:3306,
+	user:'root',
+	password:"",
+	database:'xz',
+	connectionLimit:15
+});
+//导出连接池对象
+module.exports=pool;
